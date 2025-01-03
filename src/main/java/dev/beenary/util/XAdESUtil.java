3c22312937e5e2a155a51980a1cca38d6e191446
Ana Peterlic <apeterlic@gmail.com>
2025-01-03 13:21:26 +0100

    Small changes

--- src/main/java/dev/beenary/util/XAdESUtil.java
+++ src/main/java/dev/beenary/util/XAdESUtil.java
@@ -10,12 +10,12 @@ public final class XAdESUtil {
     public static final String TRANSFORM_ALGORITHM = "http://www.w3.org/2000/09/xmldsig#enveloped-signature";
     public static final String CANONICALIZATION_ALGORITHM = "http://www.w3.org/2001/10/xml-exc-c14n#";
     public static final String DIGEST_ALGORITHM = "http://www.w3.org/2001/04/xmlenc#sha256";
-    public static final String SIGN_ALGORITHM = "http://www.w3.org/2001/04/xmldsig-more#ecdsa-sha256";
+    public static final String SIGN_ALGORITHM = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
     public static final String ATTRIBUTE_ID = "Id";
     public static final String TAG_SIGNATURE = "Signature";
 
     /**
-     * Class contains only static members and should never be instantiated.
+     * Class contains only static memebers and should never be instantiated.
      */
     private XAdESUtil() {
     }
--- src/test/java/dev/beenary/xades/XAdESSignerTest.java
+++ src/test/java/dev/beenary/xades/XAdESSignerTest.java
@@ -1,14 +1,14 @@
 package dev.beenary.xades;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.w3c.dom.Document;
-
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.security.cert.X509Certificate;
 import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
 
 /**
  * Contains tests for XMl signing and verification.
@@ -24,22 +24,32 @@ class XAdESSignerTest {
 
     @Test
     void testXmlSigning() throws Exception {
-        final byte[] encoded = this.getClass().getResourceAsStream("/xades/cm1.xml").readAllBytes();
+        final byte[] encoded = this.getClass()
+            .getResourceAsStream("/xades/unsigned-message.xml")
+            .readAllBytes();
         System.setProperty("com.sun.org.apache.xml.internal.security.ignoreLineBreaks", "true");
 
-        final String certificateFilePath = getResourceFileAsString("/test.crt");
-        final String privateKeyFilePath = getResourceFileAsString("/test.p12");
-        String privateKeyPassword = "1111";
+        final String certificateFilePath = getResourceFileAsString("/certificate.cer");
+        final String privateKeyFilePath = getResourceFileAsString("/privatekey.pem");
+        String privateKeyPassword = "password";
+        
         final XAdESSigner signer = new XAdESSigner(certificateFilePath, privateKeyFilePath, privateKeyPassword);
         final List<X509Certificate> certificateList = List.of(signer.loadCertificateFile());
+
         final Document signed = signer.sign(encoded);
+
         final XAdESVerifier verifier = new XAdESVerifier();
+
         boolean valid = verifier.verify(signed, certificateList);
+
         Assertions.assertTrue(valid);
     }
 
-    String getResourceFileAsString(final String fileName) throws URISyntaxException {
+    String getResourceFileAsString(String fileName) throws URISyntaxException {
         final URL resource = XAdESSignerTest.class.getResource(fileName);
-        return Paths.get(resource.toURI()).toAbsolutePath().toString();
+        return Paths.get(resource.toURI())
+            .toAbsolutePath()
+            .toString();
     }
+
 }
